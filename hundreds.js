6bbc5023afe2d562d4771de57b5bc1fb1e7df4d8
Joseph Pham <j.pham017@gmail.com>
2018-08-15 18:28:27 +0000

    Adds conditionals exercises

---
+++ hundreds.js
@@ -0,0 +1,16 @@
+function howManyHundreds(num){
+  if(num % 100 === 0){
+    return num / 100;
+  }
+  else{
+    var remainder = num % 100;
+    num = num - remainder;
+    return num / 100;
+  }
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
